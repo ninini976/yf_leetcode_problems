a67cde70743a5bb89a244ed7cb6aef2833af9bc2
ninini976 <yangtony@umich.edu>
2017-02-28 00:07:49 -0500

    Update 281.py

--- 281.py
+++ 281.py
@@ -68,3 +68,28 @@ class ZigzagIterator(object):
 # Your ZigzagIterator object will be instantiated and called as such:
 # i, v = ZigzagIterator(v1, v2), []
 # while i.hasNext(): v.append(i.next())
+
+# an very concise answer from StefanPochmann
+'''
+ public class ZigzagIterator {
+
+    private Iterator<Integer> i, j, tmp;
+
+    public ZigzagIterator(List<Integer> v1, List<Integer> v2) {
+        i = v2.iterator();
+        j = v1.iterator();
+    }
+
+    public int next() {
+        if (j.hasNext()) { tmp = j; j = i; i = tmp; }
+        return i.next();
+    }
+
+    public boolean hasNext() {
+        return i.hasNext() || j.hasNext();
+    }
+}
+'''
+
+
+
