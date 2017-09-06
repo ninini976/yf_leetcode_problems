9a118745c532bb5e55d93ad8350eae6faed94f6d
ninini976 <yangtony@umich.edu>
2017-09-06 17:14:49 -0400

    Create 111.py

---
+++ 111.py
@@ -0,0 +1,23 @@
+# important: if one of the child is None, use minDepth of other child
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        if not root.left:
+            return self.minDepth(root.right) + 1
+        if not root.right:
+            return self.minDepth(root.left) + 1
+        else:
+            return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
