e963fa579e8d434d7e8adf16425db1a4ef9abab1
ninini976 <yangtony@umich.edu>
2017-09-06 10:50:24 -0400

    Create 108.py

---
+++ 108.py
@@ -0,0 +1,30 @@
+# Use recursion
+# use middle number to be tree head
+# left tree left part of array
+# right tree right part of array
+
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def sortedArrayToBST(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        """
+        if not nums:
+            return None
+        
+        # // is operator for floor division
+        mid = len(nums) // 2
+        
+        head = TreeNode(nums[mid])
+        head.left = self.sortedArrayToBST(nums[:mid])
+        head.right = self.sortedArrayToBST(nums[mid+1:])
+        return head
