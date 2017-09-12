9371174d848118c530f5089acc0a94c7d0865790
ninini976 <yangtony@umich.edu>
2017-09-11 21:18:56 -0400

    Create 191.py

---
+++ 191.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        count = 0
+        for i in range(32):
+            count += 1 & n
+            n = n >> 1
+        return count
