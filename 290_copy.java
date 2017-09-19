3b7a33fb7f3c77ae725c165b1cdb46f7b94ba278
ninini976 <yangtony@umich.edu>
2017-09-19 02:33:49 -0400

    Create 290_copy.java

---
+++ 290_copy.java
@@ -0,0 +1,17 @@
+// The answer is very similar to String isomorphism
+
+
+
+
+class Solution {
+    public boolean wordPattern(String pattern, String str) {
+        String[] words = str.split(" ");
+        if (words.length != pattern.length())
+            return false;
+        Map index = new HashMap();
+        for (Integer i=0; i<words.length; ++i)
+            if (index.put(pattern.charAt(i), i) != index.put(words[i], i))
+                return false;
+        return true;
+    }
+}
