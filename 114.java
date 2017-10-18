3a44223ed87c0b0cd52d232095f74fd10925908f
ninini976 <yangtony@umich.edu>
2017-10-18 15:53:12 -0700

    Create 114.java

---
+++ 114.java
@@ -0,0 +1,33 @@
+# recursion
+# insert flattened left tree to root's right child
+# insert flattened right tree to flattened left tree's end
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void flatten(TreeNode root) {
+        if (root == null) return;
+        
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        
+        root.left = null;
+        
+        flatten(left);
+        flatten(right);
+        
+        root.right = left;
+        TreeNode cur = root;
+        while (cur.right != null) cur = cur.right;
+        cur.right = right;
+    }
+
+}
