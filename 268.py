3fd3c4f1f33ba174886e62713b32fcc72c80e5a6
ninini976 <yangtony@umich.edu>
2017-11-06 10:02:37 -0500

    Create 268.py

---
+++ 268.py
@@ -0,0 +1,19 @@
+# sort first
+# then loop
+
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        
+        nums = sorted(nums)
+
+        for i in range(len(nums)):
+            if i != nums[i]:
+                return i
+        
+        return len(nums)
